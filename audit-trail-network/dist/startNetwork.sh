71830092b53cd095caee1940acae8a71a67af2ab
adamblvck <contact@adamblvck.com>
2018-05-07 15:23:56 +0200

    Updated network permissions and generated new business network archive

--- audit-trail-network/dist/startNetwork.sh
+++ audit-trail-network/dist/startNetwork.sh
@@ -1,1 +1,1 @@
-composer network start -c PeerAdmin@hlfv1 -n audit-trail-network -V 0.0.3 -A admin -S adminpw
+composer network start -c PeerAdmin@hlfv1 -n audit-trail-network -V 0.0.4 -A admin -S adminpw
--- audit-trail-network/sample-data/test-bn-connection-util.js
+++ audit-trail-network/sample-data/test-bn-connection-util.js
@@ -8,12 +8,15 @@
 const bnUtil = require('./bn-connection-util').default;
 
 // This creates the business network connection object
-// and calls connect() on it. Calls the callback method 
+// and calls connect() on it. Calls the callback method
 // 'main' with error
 bnUtil.connect(main);
 
 // Callback function passed to the BN Connection utility
 // Error has value if there was an error in connect()
+/**
+ * @param error
+ */
 function main(error){
     // 1. Check for the connection error
     if(error){
@@ -21,20 +24,20 @@ function main(error){
         process.exit(1);
     }
 
-    console.log("1. Successfully Connected !!!");
+    console.log('1. Successfully Connected !!!');
 
     // 2. Lets ping
     bnUtil.ping((response, error)=>{
         if(error){
             console.log(error);
         } else {
-            console.log("2. Received Ping Response:");
+            console.log('2. Received Ping Response:');
             console.log(response);
         }
 
         // 3. Disconnect
         bnUtil.disconnect();
 
-        console.log("3. Disconnected");
+        console.log('3. Disconnected');
     });
 }
