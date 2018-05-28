820ddfd30fe724657c3b632014f3ebea8cf7f5d9
adamblvck <contact@adamblvck.com>
2018-05-28 13:59:11 +0200

    - Updated networking model an bna (archive file) - Updated sample data scripts

--- audit-trail-network/sample-data/cards/1-delete-cards.sh
+++ audit-trail-network/sample-data/cards/1-delete-cards.sh
@@ -3,5 +3,5 @@ composer card delete -c dieter@audit-trail-network
 composer card delete -c bram@audit-trail-network
 composer card delete -c daniel@audit-trail-network
 composer card delete -c pascal@audit-trail-network
-composer card delete -c auditor1@audit-trail-network
-composer card delete -c auditor2@audit-trail-network
+composer card delete -c auditor_aiv@audit-trail-network
+composer card delete -c auditor_omgeving@audit-trail-network
--- audit-trail-network/sample-data/issue-identities.sh
+++ audit-trail-network/sample-data/issue-identities.sh
@@ -7,8 +7,8 @@ composer identity issue -u bram -a be.vlaanderen.audittrail.ParticipantCivilian#
 composer identity issue -u daniel -a be.vlaanderen.audittrail.ParticipantPublicServant#daniel -c admin@audit-trail-network
 composer identity issue -u pascal -a be.vlaanderen.audittrail.ParticipantPublicServant#pascal -c admin@audit-trail-network
 
-composer identity issue -u auditor1 -a be.vlaanderen.audittrail.ParticipantAuditor#auditor1 -c admin@audit-trail-network
-composer identity issue -u auditor2 -a be.vlaanderen.audittrail.ParticipantAuditor#auditor2 -c admin@audit-trail-network
+composer identity issue -u auditor_aiv -a be.vlaanderen.audittrail.ParticipantAuditor#auditor_aiv -c admin@audit-trail-network
+composer identity issue -u auditor_omgeving -a be.vlaanderen.audittrail.ParticipantAuditor#auditor_omgeving -c admin@audit-trail-network
 
 # move card to a different folder
 mv *.card ./cards/
